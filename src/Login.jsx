c7e41bf4c53a00c02c2a94a46f800cdb89f22a78
Arpit1815Coder <arpityadav1518@gmail.com>
2024-07-04 01:06:00 +0530

    commit

--- src/Login.jsx
+++ src/Login.jsx
@@ -5,8 +5,8 @@ import { loginUrl } from "./auth";
 function Login() {
   return (
     <div className="login">
-      <img src="https://getheavy.com/wp-content/uploads/2019/12/spotify2019-830x350.jpg" alt="Spotify Logo" />
-      <a href={loginUrl}>LOGIN WITH SPOTIFY</a>
+      <img src="/public/melodic.png" alt="" />
+      <a href={loginUrl}>LOGIN WITH MELODIC</a>
     </div>
   );
 }
