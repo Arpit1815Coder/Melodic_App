0b585addaa7f32b8f052496c5f257b13cd6aeef1
Arpit1815Coder <arpityadav1518@gmail.com>
2024-07-05 01:13:17 +0530

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,7 @@ import  { useEffect, useState } from "react";
 import Login from "./Login";
 import { getTokenFromUrl } from "./auth";
 import SpotifyWebApi from "spotify-web-api-js";
-
-import Dashboard from "./Dashboard.jsx";
+import Dashboard from "./Dashboard";
 import "./App.css";
 
 const spotify = new SpotifyWebApi();
@@ -20,20 +19,57 @@ function App() {
       setToken(_token);
       spotify.setAccessToken(_token);
 
-      spotify.getMe().then((user) => {
+      spotify.getMe().then(user => {
         console.log("User: ", user);
       });
+
+      const script = document.createElement('script');
+      script.src = 'https://sdk.scdn.co/spotify-player.js';
+      script.async = true;
+
+      script.onload = () => {
+        window.onSpotifyWebPlaybackSDKReady = () => {
+          const player = new window.Spotify.Player({
+            name: 'Web Playback SDK Quick Start Player',
+            getOAuthToken: cb => { cb(_token); },
+            volume: 0.5
+          });
+
+          player.addListener('ready', ({ device_id }) => {
+            console.log('Ready with Device ID', device_id);
+          });
+
+          player.addListener('not_ready', ({ device_id }) => {
+            console.log('Device ID has gone offline', device_id);
+          });
+
+          player.addListener('initialization_error', ({ message }) => {
+            console.error('Failed to initialize', message);
+          });
+
+          player.addListener('authentication_error', ({ message }) => {
+            console.error('Failed to authenticate', message);
+          });
+
+          player.addListener('account_error', ({ message }) => {
+            console.error('Failed to validate Spotify account', message);
+          });
+
+          player.addListener('playback_error', ({ message }) => {
+            console.error('Failed to perform playback', message);
+          });
+
+          player.connect();
+        };
+      };
+
+      document.body.appendChild(script);
     }
   }, []);
 
   return (
     <div className="app">
-      {!token ? (
-        <Login />
-      ) : (
-        <Dashboard spotify={spotify} />
-        // Replace <Player spotify={spotify} /> with <Dashboard spotify={spotify} />
-      )}
+      {!token ? <Login /> : <Dashboard spotify={spotify} />}
     </div>
   );
 }
--- src/Dashboard.jsx
+++ src/Dashboard.jsx
@@ -1,5 +1,4 @@
-// Dashboard.jsx
-import  { useEffect, useState } from 'react';
+import { useEffect, useState, useMemo } from 'react';
 import PropTypes from 'prop-types';
 import './Dashboard.css';
 
@@ -7,24 +6,34 @@ function Dashboard({ spotify }) {
   const [playlists, setPlaylists] = useState([]);
   const [selectedPlaylist, setSelectedPlaylist] = useState(null);
   const [playlistTracks, setPlaylistTracks] = useState([]);
+  const [searchQuery, setSearchQuery] = useState('');
+  const [searchResults, setSearchResults] = useState([]);
+  const [currentTrack, setCurrentTrack] = useState(null);
+  const audio = useMemo(() => new Audio(), []);
 
-  // Fetch user's playlists on component mount
   useEffect(() => {
-    spotify.getUserPlaylists().then(response => {
-      setPlaylists(response.items);
-    }).catch(error => {
-      console.error('Error fetching playlists:', error);
-    });
+    // Fetch user's playlists on component mount
+    const fetchPlaylists = async () => {
+      try {
+        const response = await spotify.getUserPlaylists();
+        setPlaylists(response.items);
+      } catch (error) {
+        console.error('Error fetching playlists:', error);
+      }
+    };
+
+    fetchPlaylists();
   }, [spotify]);
 
   // Function to fetch tracks for a selected playlist
-  const fetchPlaylistTracks = (playlistId) => {
-    spotify.getPlaylistTracks(playlistId).then(response => {
+  const fetchPlaylistTracks = async (playlistId) => {
+    try {
+      const response = await spotify.getPlaylistTracks(playlistId);
       setPlaylistTracks(response.items);
-    }).catch(error => {
+    } catch (error) {
       console.error('Error fetching playlist tracks:', error);
-      setPlaylistTracks([]); // Set empty array in case of error
-    });
+      setPlaylistTracks([]);
+    }
   };
 
   // Function to handle playlist click
@@ -33,33 +42,120 @@ function Dashboard({ spotify }) {
     fetchPlaylistTracks(playlist.id);
   };
 
+  // Function to handle search input change
+  const handleSearchChange = (event) => {
+    setSearchQuery(event.target.value);
+  };
+
+  // Function to handle search form submit
+  const handleSearchSubmit = async (event) => {
+    event.preventDefault();
+    if (searchQuery.trim() !== '') {
+      try {
+        const response = await spotify.searchTracks(searchQuery);
+        setSearchResults(response.tracks.items);
+      } catch (error) {
+        console.error('Error searching tracks:', error);
+        setSearchResults([]);
+      }
+    }
+  };
+
+  // Function to play a track
+  const playTrack = (track) => {
+    if (currentTrack) {
+      // Pause the current track if playing
+      audio.pause();
+      setCurrentTrack(null);
+    }
+
+    // Set new track to play
+    setCurrentTrack(track);
+    audio.src = track.preview_url;
+    audio.play().catch((error) => {
+      console.error('Failed to play track:', error);
+    });
+  };
+
+  // Event listener to handle track end
+  useEffect(() => {
+    const handleTrackEnd = () => {
+      setCurrentTrack(null);
+    };
+
+    audio.addEventListener('ended', handleTrackEnd);
+
+    return () => {
+      audio.removeEventListener('ended', handleTrackEnd);
+    };
+  }, [audio]);
+
   return (
     <div className="dashboard">
       <div className="sidebar">
-        <h2>Playlists</h2>
-        <ul>
-          {playlists.map(playlist => (
-            <li key={playlist.id} onClick={() => handlePlaylistClick(playlist)}>
-              {playlist.name}
-            </li>
-          ))}
-        </ul>
+        <div className="logo-section">
+          <img src="melodic.png" alt="Logo" className="logo" />
+          <h2>Melodic</h2>
+        </div>
+        <div className="search-section">
+          
+          <form onSubmit={handleSearchSubmit}>
+           
+            <input
+              type="text"
+              value={searchQuery}
+              onChange={handleSearchChange}
+              placeholder='Search for Tracks...'
+            />
+            <button type="submit"><i className="fas fa-search"></i></button>
+          </form>
+        </div>
+        <div className="playlist-section">
+          <h2>Playlists</h2>
+          <ul>
+            {playlists.map((playlist) => (
+              <li key={playlist.id} onClick={() => handlePlaylistClick(playlist)}>
+                {playlist.name}
+              </li>
+            ))}
+          </ul>
+        </div>
       </div>
       <div className="main-content">
         {selectedPlaylist && (
           <div className="playlist-tracks">
             <h2>{selectedPlaylist.name}</h2>
             <div className="grid-container">
-              {playlistTracks.map(track => (
+              {playlistTracks.map((track) => (
                 <div key={track.track.id} className="track">
-                  <img src={track.track.album.images[0].url} alt={track.track.name} />
-                  <div>
-                    {/* <h3>{track.track.name}</h3> */}
-                    {/* <p>{track.track.artists.map(artist => artist.name).join(', ')}</p> */}
+                  <img
+                    src={track.track.album.images[0]?.url || 'placeholder-url'}
+                    alt={track.track.name}
+                  />
+                  <div className="track-overlay">
+                    <button
+                      className="play-button"
+                      onClick={() => playTrack(track.track)}
+                    >
+                      <i className="fas fa-play-circle"></i>
+                    </button>
                   </div>
                 </div>
               ))}
             </div>
+          </div>
+        )}
+        {searchResults.length > 0 && (
+          <div className="search-results">
+            <h2>Search Results</h2>
+            <ul>
+              {searchResults.map((track) => (
+                <li key={track.id}>
+                  {track.name} by {track.artists.map((artist) => artist.name).join(', ')}
+                  <button onClick={() => playTrack(track)}>Play</button>
+                </li>
+              ))}
+            </ul>
           </div>
         )}
       </div>
