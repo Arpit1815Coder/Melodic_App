d9f19899aa5c757ebaae79b548399d3979f2a860
Arpit1815Coder <arpityadav1518@gmail.com>
2024-07-08 18:36:41 +0530

    comit

--- src/auth.jsx
+++ src/auth.jsx
@@ -1,28 +1,32 @@
-// src/auth.js
-export const authEndpoint = "https://accounts.spotify.com/authorize";
-const clientId = "de27af0e5ffc4194a444f02244cd7605";
-const redirectUri = "http://localhost:3000/";
+// auth.js
+
+export const authEndpoint = 'https://accounts.spotify.com/authorize';
+const clientId = process.env.REACT_APP_SPOTIFY_CLIENT_ID;
+
+const redirectUri = process.env.NODE_ENV === 'production'
+  ? process.env.REACT_APP_SPOTIFY_REDIRECT_URI_PRODUCTION
+  : process.env.REACT_APP_SPOTIFY_REDIRECT_URI;
 
 const scopes = [
-  "user-read-currently-playing",
-  "user-read-playback-state",
-  "user-modify-playback-state",
-  "user-read-private",
-  "user-read-email",
-  "playlist-read-private",
+  'user-read-private',
+  'user-read-email',
+  'playlist-read-private',
+  'playlist-read-collaborative',
+  'user-top-read',
+  // add any other scopes you need
 ];
 
 export const getTokenFromUrl = () => {
   return window.location.hash
     .substring(1)
-    .split("&")
+    .split('&')
     .reduce((initial, item) => {
-      let parts = item.split("=");
+      let parts = item.split('=');
       initial[parts[0]] = decodeURIComponent(parts[1]);
       return initial;
     }, {});
 };
 
 export const loginUrl = `${authEndpoint}?client_id=${clientId}&redirect_uri=${redirectUri}&scope=${scopes.join(
-  "%20"
+  '%20'
 )}&response_type=token&show_dialog=true`;
